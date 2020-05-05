6837dc608a32dafbdb884b52d7ede148534eb837
Johan van der Wijk <johan@vanderwijk.nl>
2020-05-05 16:19:27 +0200

    Removed viewport

--- header.php
+++ header.php
@@ -5,7 +5,6 @@
 Website development by VanderWijk Consultancy - https://vanderwijk.nl
 -->
 <meta charset='<?php bloginfo( 'charset' ); ?>' />
-<meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0' />
 <meta name='application-name' content='<?php bloginfo('name'); ?>' />
 <meta name='apple-mobile-web-app-title' content='<?php bloginfo('name'); ?>' />
 <link rel='profile' href='http://gmpg.org/xfn/11' />
