cd4e639fa27fe70733c25f70c1c3335f3231ce98
Johan van der Wijk <johan@vanderwijk.nl>
2020-05-05 16:09:49 +0200

    Favicon

--- header.php
+++ header.php
@@ -8,8 +8,6 @@ Website development by VanderWijk Consultancy - https://vanderwijk.nl
 <meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0' />
 <meta name='application-name' content='<?php bloginfo('name'); ?>' />
 <meta name='apple-mobile-web-app-title' content='<?php bloginfo('name'); ?>' />
-<link rel='icon' type='image/x-icon' href='<?php echo get_stylesheet_directory_uri(); ?>/img/favicon.ico' />
-<link rel='apple-touch-icon' sizes='144x144' href='<?php echo get_stylesheet_directory_uri(); ?>/img/touch-icon-ipad-retina.png' />
 <link rel='profile' href='http://gmpg.org/xfn/11' />
 <?php wp_head(); ?>
 </head>
